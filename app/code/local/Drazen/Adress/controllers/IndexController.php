7afcc38cbb9f24bbf7c3850c7128291fa9767d00
dpavic <drazen.pavic@gmail.com>
2014-08-05 22:55:11 +0200

    add IndexController.php
    Add indexController, and registerd in /etc/config.xml

---
+++ app/code/local/Drazen/Adress/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+class Drazen_Adress_IndexController extends Mage_Core_Controller_Front_Action
+{
+	public function indexAction()
+	{
+		echo 'test index';
+	}
+
+	public function mojametodaAction()
+	{
+		echo 'test Moja Metoda';
+	}
+}
